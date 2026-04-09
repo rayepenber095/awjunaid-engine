0159e03ab22c27946267321c69fb818c6f40559d
Raye Penber <rayepenber095@proton.me>
2026-04-09 15:38:42 +0500

    Create colors.sh

---
+++ lib/colors.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Color definitions
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[1;33m'
+export BLUE='\033[0;34m'
+export MAGENTA='\033[0;35m'
+export CYAN='\033[0;36m'
+export WHITE='\033[1;37m'
+export NC='\033[0m'
+
+export BOLD='\033[1m'
+export DIM='\033[2m'
+export UNDERLINE='\033[4m'
